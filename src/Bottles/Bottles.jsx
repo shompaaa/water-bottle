3c452896361227529edd3dea0c38d003a508f6ee
shompaaa <shompaakter334@gmail.com>
2026-03-08 14:41:41 +0600

    json data updated

--- src/Bottles/Bottles.jsx
+++ src/Bottles/Bottles.jsx
@@ -1,9 +1,17 @@
-import React, { use, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import Bottle from "../Bottle/Bottle";
 import "./Bottles.css";
+import { getStoreCart } from "../utils/localStorage";
 
 const Bottles = ({ bottlesPromise }) => {
   const bottles = use(bottlesPromise);
+
+  //Use Effect
+
+  useEffect(()=>{
+    const storedCartIds = getStoreCart();
+    console.log(storedCartIds);
+  },[])
 
   const [cart, setCart] = useState([]);
 
---
+++ src/utils/localStorage.js
@@ -0,0 +1,33 @@
+/***
+ * 1. To get something from local storage, you will get it as string
+ * 2. Convert this string to js array/object with (JSON.stringify)
+ * 3. Then parse the stringified value for using as javaScript
+ */
+
+const getCartFromLocalStorage = () => {
+  const storedCartString = localStorage.getItem("cart");
+
+  if (storedCartString) {
+    const storedCart = JSON.parse(storedCartString);
+    return storedCart;
+  }
+  return [];
+};
+
+const saveCartToLocalStorage = (cart) => {
+  const cartStringified = JSON.stringify(cart);
+  localStorage.setItem("cart", cartStringified);
+};
+
+const addItemToCartLocalStorage = (id) => {
+  const cart = getCartFromLocalStorage();
+  cart.push(id);
+
+  //Save cart to the local storage
+  saveCartToLocalStorage(cart);
+};
+
+export {
+  getCartFromLocalStorage as getStoreCart,
+  addItemToCartLocalStorage as addToScoreCart,
+};
